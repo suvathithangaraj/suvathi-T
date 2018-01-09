144e99b7d6244a89f606d31a51914c84d144a603
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-01-09 19:30:43 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter any character");
+scanf("%c,"&ch);
+if(ch=='a'!!ch=='e'!!ch=='i'!!ch=='o'!!ch=='u'!!ch=='A'!!ch=='E'!!ch=='I'!!ch=='O'!!ch=='U')
+{
+printf("enter character in vowels");
+}
+else
+{
+printf("enter character in contants");
+}
+else
+{
+getch()
+}
