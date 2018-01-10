f0b53df74160ef5fe595b5f8d903e18fda77f9ac
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-01-10 19:30:03 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter three number");
+scanf("%d%d%d",&a,&b,&c);
+if(a>=b&&a>=c)
+{
+printf("a is largest");
+}
+if(b>=a&&b>=c)
+{
+printf("b is largest");
+}
+if(c>=a&&c>=b)
+{
+printf("c is largest");
+}
+return 0;
+}
