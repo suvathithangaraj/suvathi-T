9f176e05755a2ce9e81bd6a0f00e790a084a3fe4
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-01-19 16:21:01 +0530

    Create odd numbers.c

---
+++ odd numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,f,l
+printf("enter the first interval");
+scanf("%d",&f);
+printf("enter the second number");
+scanf("%d",&f);
+for(i=f;i<=l;i++)
+{
+if(i%2==1)
+{
+printf("%d is an odd no";);
+}
+}
+return 0;
+}
