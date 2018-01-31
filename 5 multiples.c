6914c2b15c0e5dd707c234f0a46de1cf09257f7a
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-01-31 16:31:16 +0530

    Create 5 multiples.c

---
+++ 5 multiples.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+        int i,a;
+        printf("\a Please Give The Value of N:");
+        scanf("%d",&n);
+        for(i=1;i<=a;i++)
+        {
+            printf("\n 5 * %d = %d",i,5*i);
+        }
+}
