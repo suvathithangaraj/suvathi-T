886c68a9d2c0482d2680200f44d31c842756849e
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-01-31 16:51:10 +0530

    Create even number.c

---
+++ even number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the first number":\n);
+scanf("%d",\n);
+printf("enter the last number"\n);
+scanf("%d",\n);
+prntf("enter the even number between%d%d":a,b);
+}
+for(i=a;i<=b;i++)
+{
+if(c==0);
+c%=a
+return 0;
+}
