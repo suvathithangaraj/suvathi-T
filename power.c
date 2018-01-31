103d50978a91f6cf36739e15eb1497144835d364
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-01-31 16:39:23 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+int pow,num,sum=1;
+printf("\n enter the number");
+scanf("%d",&num);
+printf("\n enter power of a number");
+scanf("%d",&pow);
+while(i<=power)
+{
+sum=sum*num;
+i++;
+}
+printf("\n %d to the power is %d:%1d,num,sum);
+return 0;
+}
