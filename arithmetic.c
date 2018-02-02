ef7c6e58afce9586562a8b410b80da9c304ea853
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-02 16:35:34 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,10 @@
+include<stdio.h>
+int main()
+{
+float n,i,s,a;
+scanf("%d%d%d",&a,&n,&i);
+printf("enter the arithmetic series of the value",a,n,i);
+s=(n/2)*(2*a+(n-1)*d);
+printf("sum of arithmetic progression"\n);
+return 0;
+}
