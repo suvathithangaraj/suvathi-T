cb4b9d2c97963c45db040b0a8b2fb2b527a244ba
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-02 16:17:56 +0530

    Create number N.c

---
+++ number N.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,i,n;
+printf("enter the number"\n);
+scanf("%d",&n);
+for(i=0;i<=(n+1);i++);
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=(n+1);i++)
+{
+if(a[i]>first)  
+{
+first=a[i]    
+}
+return 0;
+}
+}
