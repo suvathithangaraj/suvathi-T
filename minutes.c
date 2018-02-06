73bbf20b3fd77fb9619f216da51e75ec469fbc83
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-06 19:06:23 +0530

    Create minutes.c

---
+++ minutes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int mins,hrs,rmin;
+printf("enter the minutes");
+scanf("%d",&minutes);
+hrs=mins/60;
+rmin=mins%60;
+printf("%d %d",hrs,rmin);
+return 0;
+}
