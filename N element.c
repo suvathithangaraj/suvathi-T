b2cd90f2aa2e169227bdf6eb96a908538953e320
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-07 16:56:00 +0530

    Create N element.c

---
+++ N element.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[3],i,n;
+printf("enter the value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d %d",i,a[i]:\n);
+}
+return 0;
+}
