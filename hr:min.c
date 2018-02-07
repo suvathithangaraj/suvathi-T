61bc0670f34d3cd539c02db1fbe97958fcd01016
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-07 16:13:12 +0530

    Create hr:min.c

---
+++ hr:min.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int hr1,hr2,m1,m2,res,tot;
+printf("Enter hr1 and m1"\n);
+scanf("%d%d",&hr1,&m1);
+printf("Enter hr2 and m2":\n);
+scanf("%d%d",&hr2,&m2);
+res=hr1-hr2;
+tot=m1-m2;
+printf("%d %d",res,tot);
+return 0;
+}
