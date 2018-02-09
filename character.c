f626d2ec1db2306019766b6eff25c7b547e1bf7d
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-09 15:59:17 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[50]
+int i=0,cout=0;
+printf("enter the string");
+scanf("%d",&str);
+while(str[i]!='\0')
+if(!(str[i]=="str"))
+{
+++count;
+}
+++i;
+}
+printf("no of characters is %d ",count);
+}
