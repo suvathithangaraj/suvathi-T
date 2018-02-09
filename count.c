e7eb60bba8a95824a8082a87a755b1a3853ba70d
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-09 16:23:25 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[100]
+int=0,word=0;
+printf("enter the number of string");
+scanf("%d",&n);
+gets(string)
+while(string[i]!='\0')
+if(string[i]==' '\\ string[i]=='\n'\\ string[i]=='\t')
+{
+i++    
+}
+printf("enter the total number of string"\t);
+return 0;
+}
