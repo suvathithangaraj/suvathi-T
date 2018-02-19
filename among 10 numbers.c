9b4f3f1848573ba9bdc87516234ab0ad5a24d852
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-19 16:50:02 +0530

    Create among 10 numbers.c

---
+++ among 10 numbers.c
@@ -0,0 +1,18 @@
+ int a[10];
+    int i;
+    int g;
+        for(i=0;i<10;i++) 
+        {
+    scanf("%d",&a[i]);
+    }
+        g=a[0];
+    for(i=0;i<10;i++) 
+    {
+if(a[i]>g)
+{
+g=a[i];
+    }
+    }
+    printf("Greatest of ten numbers is %d",g);
+    return 0;
+  }
