7c5f2071fdda1321b9619b28aba3f855203cd19d
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-21 16:14:46 +0530

    Create string k.c

---
+++ string k.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str[50];
+int n,i;
+printf("enter the number of  string");
+scanf("%s",&str);
+printf("enter the string");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("%s",str);
+}
+}
