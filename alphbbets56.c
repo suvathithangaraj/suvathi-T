482d447a2f96683d5be1239660ed12df179e8180
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-02-26 16:46:18 +0530

    Create alphbbets56.c

---
+++ alphbbets56.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=0,count1=0;
+	char a[10];
+    printf("Enter the string:");
+    scanf("%s",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
+        {
+            count++;
+        }
+        else if(a[i]>='0'&&a[i]<='9')
+        {
+            count1++;
+        }
+    }
+    if(count>0&&count1>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
