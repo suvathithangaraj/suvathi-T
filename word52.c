fea7625887f0b2a937b226be0f00f5915399092d
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-03-02 16:06:55 +0530

    Create word52.c

---
+++ word52.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+switch(n)
+{
+case1:printf("one");
+break;
+case2:printf("two");
+break;
+case3:printf("three");
+break;
+case4:printf("four");
+break;
+case5:printf("five");
+break;
+case6:printf("six");
+break;
+case7:printf("seaven");
+break;
+case8:printf("eight");
+break;
+case9:printf("nine");
+break;
+case10:printf("ten");
+break;
+default:printf("enter the limit between 1to 10");
+}
+return 0;
+}
+
+    
