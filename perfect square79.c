33947da91d3f5d77a57a898774e882247480fafa
suvathithangaraj <32239513+suvathithangaraj@users.noreply.github.com>
2018-03-19 16:44:46 +0530

    Create perfect square79.c

---
+++ perfect square79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    if(a==b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("not");
+    }
+    returnn 0;
+}
